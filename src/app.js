24d67ba7506f36e6ee3714924436abd65f981ac9
caoquyen1913 <caoquyen1913@gmail.com>
2021-11-10 15:23:39 +0700

    add log morgan, create doc api

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ import express from 'express';
 import mongooseConfig from './config/mongoose.config';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import morgan from 'morgan';
 import './cronJobs';
 import responseHandle from './utils/responseHandle';
 import bodyParser from 'body-parser';
 import apiRouter from './api/router';
+import docRouter from './docs/docApi.router';
 import { HttpStatusCode } from './const/httpCode';
-import {limiter} from "./config/rateLimit.config"
+import { limiter } from './config/rateLimit.config';
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(limiter)
+app.use(limiter);
+app.use(morgan('combined'));
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -23,6 +26,20 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1', apiRouter);
+app.use('/docs', docRouter);
+app.use((err, req, res, next) => {
+  return responseHandle.send(
+    res,
+    err.status || HttpStatusCode.INTERNAL_SERVER,
+    {
+      errors: [
+        {
+          error: err.message,
+        },
+      ],
+    }
+  );
+});
 
 app.all('*', (req, res) => {
   return responseHandle.send(res, HttpStatusCode.NOT_FOUND, {
---
+++ src/docs/docApi.router.js
@@ -0,0 +1,25 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import express from 'express';
+import swaggerDocument from './docApi.json';
+const router = express.Router();
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API',
+      description: '',
+      contact: {
+        name: 'dev',
+      },
+    },
+    severs: ['http://localhost:5000/api/v1'],
+  },
+  apis: ['../app.js'],
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerDocs));
+export default router;
